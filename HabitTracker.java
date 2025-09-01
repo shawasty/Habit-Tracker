22d3e049a9201d06541a29b0208983815ae94f0c
shawasty <shawasty@gmail.com>
2025-08-31 21:40:19 -0400

    created Habit tracker  classes

--- HabitTracker.java
+++ HabitTracker.java
@@ -12,8 +12,37 @@ public class HabitTracker {
     // add a habit to the List
     public void addHabit(Habit h) {
         habits.add(h);
-        
+
     }
+    // find a habit by its name (case-insensitive)
+    public Habit findByName(String name) {
+        for (Habit h : habits) {
+            if (h.getName().equalsIgnoreCase(name)) {
+                return h;
+                
+            }
+            
+        }
+        // return null of not found
+        return null;       
+    }
+
+    //list all habits to the console
+    public void listHabits(){
+        if(habits.isEmpty()){
+            System.out.println("No habits found");
+            // exit the method
+            return;
+        }
+        // declare a number for display
+        int i = 1;
+
+        for (Habit h : habits) {
+            System.out.println(i++ + ". "+ h);
+            
+        }
+    }
+   
 
 
 }
